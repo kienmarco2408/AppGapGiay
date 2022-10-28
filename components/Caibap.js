4d93961859f95b12b98ece371ab6edbad2accc26
kienmarco2408 <kienmarco999@gmail.com>
2022-10-28 18:58:02 +0700

    update

--- components/Caibap.js
+++ components/Caibap.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -7,31 +7,40 @@ import {
   StatusBar,
   TouchableHighlight,
   Image,
-  TouchableOpacity
-} from "react-native";
+  TouchableOpacity,
+} from 'react-native';
 
-function CaiBap({navigation}){
-    return(
-        <View style={styles.MainContainer}>
-        <TouchableOpacity onPress={()=> navigation.navigate("C2")} >
-          <Image source={require("../assets/c1.png")} />
-        </TouchableOpacity>
+function CaiBap({ navigation }) {
+  return (
+    <View style={styles.MainContainer}>
+      <View style={{ marginLeft: 40, marginTop: 20 }}>
+        <Image
+          style={{ width: '15%', height: 40 }}
+          source={require('../assets/happy.png')}
+        />
       </View>
-    )
+      <TouchableOpacity onPress={() => navigation.navigate('C2')}>
+        <Image
+          source={require('../assets/c1.png')}
+          style={{ borderRadius: 20, borderColor: '#FFAC30', borderWidth: 5 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    MainContainer: {
-      justifyContent: "center",
-      alignItems: "center",
-      flex: 1,
-      backgroundColor: "#ade2e6",
-    },
-  
-    imageStyle: {
-      width: 300,
-      height: 300,
-    },
-  });
+  MainContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#ade2e6',
+  },
 
-  export default CaiBap;
+  imageStyle: {
+    width: 300,
+    height: 300,
+  },
+});
+
+export default CaiBap;
