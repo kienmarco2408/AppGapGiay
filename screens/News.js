686e05fb1ce5361a7ff75c7542790382e3af96df
kienmarco2408 <kienmarco999@gmail.com>
2022-10-28 17:32:53 +0700

    update

--- screens/News.js
+++ screens/News.js
@@ -7,39 +7,67 @@ import {
   ImageBackground,
   StatusBar,
   TouchableOpacity,
-  ScrollView,
   Alert,
 } from 'react-native';
 import { Button, Searchbar, TextInput } from 'react-native-paper';
+import { ScrollView } from 'react-native-virtualized-view';
 import NewDetail from '../components/NewDetail';
 
 const News = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.background}>
-        <View
-          style={{
-            marginLeft: 40,
-            marginTop: 20,
-          }}
-        >
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        <View style={styles.background}>
+          <View
+            style={{
+              marginLeft: 40,
+              marginTop: 20,
+            }}
+          >
+            <Image
+              style={{ width: '15%', height: 40 }}
+              source={require('../assets/happy.png')}
+            />
+          </View>
+
+          <View
+            style={{
+              width: '90%',
+              height: '90%',
+              backgroundColor: 'white',
+              marginHorizontal: '5%',
+              marginTop: 20,
+              borderRadius: 20,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: '700',
+                marginTop: 29,
+                alignSelf: 'center',
+              }}
+            >
+              Văn Hóa Origame
+            </Text>
+            <NewDetail />
+          </View>
+        </View>
+        <View style={{ marginTop: '15%', justifyContent: 'center' }}>
           <Image
-            style={{ width: '15%', height: 40 }}
-            source={require('../assets/happy.png')}
+            source={require('../assets/hoatiet.png')}
+            style={{ alignItems: 'center', width: '110%' }}
           />
         </View>
-
-        <View style={{}}>
-          <NewDetail />
-        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ade2e6',
+    height: '100%',
   },
   background: {
     flex: 1,
