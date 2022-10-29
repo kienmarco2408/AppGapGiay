7a0bb278eff7143a48cf334681140e4eae8a3b40
kienmarco2408 <kienmarco999@gmail.com>
2022-10-29 11:41:35 +0700

    update

--- screens/Users.js
+++ screens/Users.js
@@ -1,3 +1,4 @@
+import { Entypo, FontAwesome, Ionicons } from '@expo/vector-icons';
 import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
@@ -15,98 +16,114 @@ import { Button, TextInput } from 'react-native-paper';
 const Users = () => {
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={{ marginTop: StatusBar.currentHeight }}
-        contentContainerStyle={{ paddingBottom: '15%' }}
-      >
-        <View style={styles.background}>
+      <View style={styles.background}>
+        <View>
           <View
             style={{
-              alignItems: 'center',
-              justifyContent: 'center',
+              marginLeft: 40,
+              marginTop: 20,
             }}
+          >
+            <Image
+              style={{ width: '15%', height: 40 }}
+              source={require('../assets/happy.png')}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: 'auto',
+            width: '85%',
+            backgroundColor: 'white',
+            marginTop: '5%',
+            alignSelf: 'center',
+            paddingBottom: 20,
+            borderRadius: 20,
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: '700',
+                marginTop: 29,
+                alignSelf: 'center',
+              }}
+            >
+              Cá Nhân
+            </Text>
+          </View>
+          <View>
+            <Image
+              source={require('../assets/girl.png')}
+              style={{ borderRadius: 60, alignSelf: 'center', marginTop: 20 }}
+            />
+          </View>
+          <View style={{ alignSelf: 'center', marginTop: 10 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <FontAwesome name="phone" size={24} color="#FFAC30" />
+              <Text style={{ fontSize: 12, marginLeft: 15 }}>
+                (+33)6 55 59 16 45
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: 10,
+              }}
+            >
+              <Ionicons name="mail-outline" size={24} color="#FFAC30" />
+              <Text style={{ fontSize: 12, marginLeft: 15 }}>
+                janecooper@gmail.com
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: 10,
+              }}
+            >
+              <Ionicons name="location-sharp" size={24} color="#FFAC30" />
+              <Text style={{ fontSize: 12, marginLeft: 15 }}>
+                29 Vũ Phạm Hàm - Cầu Giấy - Hà Nội
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{ alignSelf: 'center', marginTop: 30, paddingBottom: 40 }}
           >
             <View
               style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'row',
+                height: 46,
+                width: 248,
+                borderRadius: 35,
+                backgroundColor: '#FFAC30',
               }}
             >
-              <Image
-                style={{ width: '15%', height: 40, left: '-340%' }}
-                source={require('../assets/happy.png')}
-              />
-            </View>
-
-            <View style={{ top: '10%' }}>
-              <View
+              <Text
                 style={{
-                  width: 319,
-                  height: 414,
-                  backgroundColor: '#FFFFFF',
-                  borderWidth: 1,
-                  borderColor: '#FFAC30',
-                  borderRadius: 6,
+                  color: 'white',
+                  alignSelf: 'center',
+                  marginTop: 14,
+                  fontSize: 16,
+                  fontWeight: '700',
                 }}
               >
-                <View
-                  style={{
-                    top: '5%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Text
-                    style={{ color: 'black', fontWeight: '700', fontSize: 25 }}
-                  >
-                    Cá Nhân{' '}
-                  </Text>
-
-                  <Image
-                    source={require('../assets/girl.png')}
-                    style={{ borderRadius: 70 }}
-                  />
-
-                  <View
-                    style={{
-                      top: '10%',
-                      width: 301,
-                      height: 56,
-                      backgroundColor: '#FFAC30',
-                      borderWidth: 1,
-                      borderColor: '#FFAC30',
-                      borderRadius: 6,
-                    }}
-                  >
-                    <View
-                      style={{
-                        top: '5%',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <Text
-                        style={{
-                          color: '#fff',
-                          fontWeight: '700',
-                          fontSize: 25,
-                        }}
-                      >
-                        Edit Profile
-                      </Text>
-                    </View>
-                  </View>
-                </View>
-              </View>
+                Edit Profile
+              </Text>
             </View>
           </View>
         </View>
+      </View>
 
-        <View style={{ top: '10%' }}>
-          <Image source={require('../assets/hoatiet.png')} />
-        </View>
-      </ScrollView>
+      <View>
+        <Image
+          source={require('../assets/hoatiet.png')}
+          style={{ width: '110%' }}
+        />
+      </View>
     </View>
   );
 };
